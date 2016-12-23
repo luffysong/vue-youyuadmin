e7534fa30e2c0a3e2b6efa1c661b1783f99d4d1b
sweeetcc <sweeetcc@gmail.com>
2016-12-23 16:32:13 +0800

    Basic components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/**
+ * External dependencies
+ */
 import Vue from 'vue';
+import Vuex from 'vuex';
+import Element from 'element-ui';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+/**
+ * Internal dependencies
+ */
 import App from './App';
+
+Vue.use(Vuex);
+Vue.use(Element);
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+if () {
+
+}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
+  template: '<App />',
   components: { App },
 });
