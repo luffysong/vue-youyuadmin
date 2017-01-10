b67a0037089ab492443a63e1f045385655f9807b
小包 <luxinyue@36kr.com>
2017-01-10 10:51:01 +0800

    fix: 修改文件名

---
+++ src/containers/TransferDetail/forms/pending.js
@@ -0,0 +1,37 @@
+export default {
+  name: 'TransferFormPending',
+  props: ['form'],
+  data() {
+    console.log(this.form, 'form');
+    return {
+      id: this.form.data.id,
+      projectName: this.form.data.projectName,
+      maker: this.form.data.maker,
+      transferor: this.form.data.transferor,
+      listedShare: this.form.data.listedShare,
+      listedPrice: this.form.data.listedPrice,
+      applyTime: this.form.data.applyTime,
+      listedDays: this.form.data.listedDays,
+      assignee: this.form.data.assignee,
+      assigneeCard: this.form.data.assigneeCard,
+      rules: {
+        id: [
+          { required: true, message: '请输入份额ID', trigger: 'blur' },
+        ],
+        projectName: [
+          { required: true, message: '请输入项目名称', trigger: 'blur' },
+        ],
+      },
+    };
+  },
+  methods: {
+    handleReset() {
+      this.$refs.form.resetFields();
+    },
+    handleSubmit() {
+      this.$refs.form.validate((valid) => {
+        console.log(valid, 'v');
+      });
+    },
+  },
+};
