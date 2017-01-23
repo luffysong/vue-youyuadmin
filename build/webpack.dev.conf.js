f6b07c1e8e2586bb38f0580ad8d18735b76d26a6
sweeetcc <sweeetcc@gmail.com>
2017-01-23 16:13:52 +0800

    feat: source map update

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -17,7 +17,7 @@ module.exports = merge(baseWebpackConfig, {
     loaders: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
   // eval-source-map is faster for development
-  devtool: '#inline-source-map',
+  devtool: '#eval-source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': config.dev.env
