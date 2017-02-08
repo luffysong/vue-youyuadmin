953de607c897ee88b5ca9b47ea0c5911ed262000
小包 <luxinyue@36kr.com>
2017-02-08 11:41:05 +0800

    feat: 部署脚本

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-npm run build
+npm run build:$2;
+
+pathstr="/data/docker/$2/work/frontend/prod/36jr/youyuadmin/dist";
+
+ssh ali-rong-$1 "mkdir -p $pathstr";
+
+rsync -rvltOD ./dist/* "www@ali-rong-$1:$pathstr";
+
+
+
