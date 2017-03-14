86ccc680b5a0e10ebeda682b0831172ed851391c
小包 <luxinyue@36kr.com>
2017-03-14 14:56:48 +0800

    fix: 上传图片 获取token 地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@ const ENV = process.env.API_ENV;
 module.exports = {
   NODE_ENV: '"production"',
   API_HOST: `"http://admin${ ENV === 'prod' ? '' : ENV }.corp.youyutouzi.com"`,
-  UPLOAD_TOKEN: `"http://${ ENV === 'prod' ? '' : ENV }.youyutouzi.com"`,
+  UPLOAD_TOKEN: `"http://${ ENV === 'prod' ? 'www' : ENV }.youyutouzi.com"`,
 }
