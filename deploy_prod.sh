4ca7ccb1f3e146800b2f8c1e2aef586a9094379e
小包 <luxinyue@36kr.com>
2017-03-14 13:40:45 +0800

    chore: 有娱后台部署服务器更换

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -6,29 +6,29 @@ npm run build
 cdnstr="/data/work/asset/youyuadmin"
 
 #site1
-rsync -rvltOD ./dist/* ali-rong-proxy-01:$cdnstr
+rsync -rvltOD ./dist/* ali-rong-proxy-03:$cdnstr
 rsync -rvltOD ./dist/* ali-rong-api-01:$cdnstr
 
 #site2
-rsync -rvltOD ./dist/* ali-rong-proxy-02:$cdnstr
+rsync -rvltOD ./dist/* ali-rong-proxy-04:$cdnstr
 rsync -rvltOD ./dist/* ali-rong-api-02:$cdnstr
 
 ###### 业务逻辑
 pathstr="/data/work/frontend/prod/youyuadmin/dist"
-if [ $1 = 'site1' ]; then
-    ssh ali-rong-proxy-01 "mkdir -p $pathstr"
-    rsync -rvltOD ./dist/* ali-rong-proxy-01:$pathstr
-elif [ $1 = 'site2' ]; then
-    ssh ali-rong-proxy-02 "mkdir -p $pathstr"
-    rsync -rvltOD ./dist/* ali-rong-proxy-02:$pathstr
+if [ $1 = 'site3' ]; then
+    ssh ali-rong-proxy-03 "mkdir -p $pathstr"
+    rsync -rvltOD ./dist/* ali-rong-proxy-03:$pathstr
+elif [ $1 = 'site4' ]; then
+    ssh ali-rong-proxy-04 "mkdir -p $pathstr"
+    rsync -rvltOD ./dist/* ali-rong-proxy-04:$pathstr
 elif [ $1 = 'sim' ]; then
     ssh ali-rong-sim-01 "mkdir -p $pathstr"
     rsync -rvltOD ./dist/* ali-rong-sim-01:$pathstr
 elif [ $1 = 'all' ]; then
-    ssh ali-rong-proxy-01 "mkdir -p $pathstr"
-    ssh ali-rong-proxy-02 "mkdir -p $pathstr"
+    ssh ali-rong-proxy-03 "mkdir -p $pathstr"
+    ssh ali-rong-proxy-04 "mkdir -p $pathstr"
     ssh ali-rong-sim-01 "mkdir -p $pathstr"
-    rsync -rvltOD ./dist/* ali-rong-proxy-01:$pathstr
-    rsync -rvltOD ./dist/* ali-rong-proxy-02:$pathstr
+    rsync -rvltOD ./dist/* ali-rong-proxy-03:$pathstr
+    rsync -rvltOD ./dist/* ali-rong-proxy-04:$pathstr
     rsync -rvltOD ./dist/* ali-rong-sim-01:$pathstr
 fi
